8e47383a05792bd199518a83c56c1b0bbe1bf70b
Nico DiVario <102742439+nad328@users.noreply.github.com>
2023-06-14 23:51:59 -0400

    ftgy

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,5 +9,14 @@ import { Physics2DPlugin } from "gsap/Physics2DPlugin";
 
 let mainTL = gsap.timeline();
 
+function color(){
+    let tl = gsap.timeline();
+
+    tl.to("#owl", { backgroundColor: "#3c5350" })
+
+    return tl;
+
+}
+
 
 GSDevTools.create();
