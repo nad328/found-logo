eac80fd5fe32fb1109242805681f55d90a71b9d2
Nico DiVario <102742439+nad328@users.noreply.github.com>
2023-06-15 01:20:43 -0400

    vjhbkjnl

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,8 +14,8 @@ function draw(){
     let tl = gsap.timeline();
 
     tl.to("#right" , {scaleY: 0 , duration: 3})
-    tl.to("#left" , {scaleY: 0 , duration: 3} '-=2')
-    tl.to("#bottom" , {scaleY: 0 , duration: 3} '-=1')
+    tl.to("#left" , {scaleY: 0 , duration: 3} , '-=2.6')
+    tl.to("#bottom" , {scaleY: 0 , duration: 3} , '-=2.3')
 
 
 
@@ -23,8 +23,29 @@ function draw(){
 
 }
 
-let mainTL = gsap.timeline();
+function letters(){
+    let tl = gsap.timeline();
 
+    tl.to("#m , #a , #g , #i , #c" , {scale: 0 , duration: .1})
+
+    tl.to("#Rectangle_3" , {opacity: 0 , duration: .5})
+
+    tl.to("#m , #a , #g , #i , #c" , {scale: 5 , duration: .3 , ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: .5 , duration: .3})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 3 , duration: .3})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 1 , duration: .3})
+
+
+
+    return tl;
+
+}
+
+
+let mainTL = gsap.timeline();
+mainTL.add(letters());
 mainTL.add(draw());
+
+
 
 GSDevTools.create();
