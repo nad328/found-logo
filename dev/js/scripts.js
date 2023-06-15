2251a6da95aa2232a8681b71512adff1630a45ad
Nico DiVario <102742439+nad328@users.noreply.github.com>
2023-06-15 01:41:01 -0400

    fdhgcjhvkbjknlm,

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,6 +7,13 @@ import { Physics2DPlugin } from "gsap/Physics2DPlugin";
 
 gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, Physics2DPlugin);
 
+gsap.set("#m", {transformOrigin:"center"})
+gsap.set("#a", {transformOrigin:"center"})
+gsap.set("#g", {transformOrigin:"center"})
+gsap.set("#i", {transformOrigin:"center"})
+gsap.set("#c", {transformOrigin:"center"})
+
+
 
 
 
@@ -28,13 +35,39 @@ function letters(){
 
     tl.to("#m , #a , #g , #i , #c" , {scale: 0 , duration: .1})
 
+    tl.to(" #g " , {rotate: -20 })
+
     tl.to("#Rectangle_3" , {opacity: 0 , duration: .5})
 
-    tl.to("#m , #a , #g , #i , #c" , {scale: 5 , duration: .3 , ease: 'linear'})
-    tl.to("#m , #a , #g , #i , #c" , {scale: .5 , duration: .3})
-    tl.to("#m , #a , #g , #i , #c" , {scale: 3 , duration: .3})
-    tl.to("#m , #a , #g , #i , #c" , {scale: 1 , duration: .3})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 10 , duration: .3 , ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: .5 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 5 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: .8 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 3 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: .8 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 1.2 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: .9 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 1.1 , duration: .3, ease: 'linear'})
+    tl.to("#m , #a , #g , #i , #c" , {scale: 1 , duration: .3, ease: 'linear'})
+  
 
+
+    return tl;
+
+}
+
+function rattle(){
+    let tl = gsap.timeline();
+
+    tl.to("#g" , {rotate: -22, duration: .1 })
+    tl.to("#g" , {rotate: -18, duration: .1 })
+    tl.to("#g" , {rotate: -21.5, duration: .1 })
+    tl.to("#g" , {rotate: -18.5, duration: .1 })
+    tl.to("#g" , {rotate: -21, duration: .1 })
+    tl.to("#g" , {rotate: -19, duration: .1 })
+    tl.to("#g" , {pause: .5})
+    tl.to("#g" , {rotate: 0, duration: .1 })
+  
 
 
     return tl;
@@ -45,6 +78,7 @@ function letters(){
 let mainTL = gsap.timeline();
 mainTL.add(letters());
 mainTL.add(draw());
+mainTL.add(rattle());
 
 
 
